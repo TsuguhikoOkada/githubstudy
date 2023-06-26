f2193d73de07b6aa1b06968ef8a22f363f18862f
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2023-06-26 17:10:59 +0900

    test

--- learn-github-actions.py
+++ learn-github-actions.py
@@ -1,13 +1,12 @@
-# Python script: save_message.py
 from github import Github
 import os
 from datetime import datetime
- 
+
 g = Github(os.getenv("GH_TOKEN"))
 repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
-commit = repo.get_commit(os.getenv("GITHUB_SHA"))
+commit = repo.get_commit(os.getenv("COMMIT_SHA"))
 message = commit.commit.message
- 
+
 filename = datetime.now().strftime("%Y%m%d_%H%M%S.md")
 with open(filename, "w") as f:
-    f.write(f"# {message}")
+    f.write(f"Last commit: {message}")
