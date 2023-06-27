9b27ce462cdd12c928f1c50bee43766365e15b1e
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2023-06-27 21:28:39 +0900

    test

--- add_commit_to_md.py
+++ add_commit_to_md.py
@@ -1,24 +1,12 @@
-# add_commit_to_md.py
 from github import Github
-import os
 from datetime import datetime
+import os
 
-# GitHub APIを利用するための認証
-g = Github(os.getenv("GITHUB_TOKEN"))
-
-# 現在のリポジトリを取得
+g = Github(os.getenv("GH_TOKEN"))
 repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
-
-# 最新のコミットを取得
-commit = repo.get_commit(sha="develop") # develop branch
+commit = repo.get_commit(os.getenv("GITHUB_SHA"))
 message = commit.commit.message
 
-# 現在の日時を取得
-now = datetime.now()
-
-# ファイル名を生成 (yyyymmdd_hhmmss.md)
-filename = now.strftime("%Y%m%d_%H%M%S.md")
-
-# 最新のコミットメッセージをMarkdownファイルに保存
-with open(filename, "w") as f:
-    f.write(f"{message}\n")
+date_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+with open(f"{date_str}.md", "w") as f:
+    f.write(message)
